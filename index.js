6411abf269f433f9c72c15e2330b73fc631bf4fc
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-02-28 19:49:31 +0530

    Made student API for second Year students

---
+++ index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const student_router = require("./routes/student_routes");
+const connectDB = require("./MONGODB/DB_connections");
+
+const PORT = process.env.PORT || 8100;
+
+//! Direct Route Addressing.
+app.get("/", (req, res) => {
+  res.send("I am Live and running but might be down later");
+});
+//! Route Addressing through middleware.
+app.use("/api/students", student_router);
+
+const start = async () => {
+  await connectDB(process.env.MONGODB_URL);
+  app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+  });
+};
+
+start();
