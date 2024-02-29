0b54da5ffb1472e591994f79ae78006a999775ee
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-02-29 19:45:40 +0530

    added whole 2nd year data

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use("/api/students", student_router);
 
 const start = async () => {
   await connectDB(process.env.MONGODB_URL);
-  app.listen(PORT, "0.0.0.0", () => {
+  app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
   });
 };
